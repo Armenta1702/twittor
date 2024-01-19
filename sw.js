62c7bb497987f3eac5ddddf673afbb943a7c9930
Armenta1702 <armenta.jenn15@gmail.com>
2024-01-18 17:43:19 -0700

    Modificaciones para IPhone X+

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 //Importaciones 
 importScripts('js/libs/sw-utils.js');
 
-const STATIC_CACHE = "static-v3";
-const DYNAMIC_CACHE = "dynamic-v1";
+const STATIC_CACHE = "static-v4";
+const DYNAMIC_CACHE = "dynamic-v2";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
@@ -49,6 +49,11 @@ self.addEventListener('activate', e => {
             if(key !== STATIC_CACHE && key.includes('static')){
                 return caches.delete(key);
             }
+
+            // dynamic-vN
+            if(key !== DYNAMIC_CACHE && key.includes('dynamic')){
+              return caches.delete(key);
+          }
         });
     });
 
